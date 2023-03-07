fae215c8a0c60e5012446be80a1cab4ec314ff86
Viktoriya <kuzmina.vika2012@yandex.ru>
2023-03-07 15:51:17 +0300

    Процент по вкладу

---
+++ Seminar3/ex4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Представим банк, в котором алгоритм начисления процента по вкладу зависит от суммы вклада. На вход будет подаваться число (сумма вклада). 
+// При значении меньше 100, будет начислено 5 %, 
+// если значение находится в диапазоне от ста до двухсот — 7 %, 
+// если больше — 10 %. Отработав, программа должна вывести общую сумму с начисленными процентами.
+
+Console.Write("Введите сумму вклада: ");
+double cash = Convert.ToDouble(Console.ReadLine());
+double result = 0;
+
+if (cash < 100)
+{
+    result = ((double)cash / 100) * 5;
+    Console.WriteLine($"Сумма вклада {cash} + 5% = {result + cash}");
+}
+else if (cash > 100 && cash < 200)
+{
+    result = ((double)cash / 100) * 7;
+    Console.WriteLine($"Сумма вклада {cash} + 7% = {result + cash}");
+}
+else if (result > 200)
+{
+    result = ((double)cash / 100) * 10;
+    Console.WriteLine($"Сумма вклада {cash} + 10% = {result + cash}");
+}
