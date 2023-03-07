aea120eb7fe07b8f0d20e288326e90d8396c22b8
Viktoriya <kuzmina.vika2012@yandex.ru>
2023-03-07 15:39:10 +0300

    Нахождение чётных чисел от 1 до N

---
+++ Seminar1/ex4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 0) {
+    Console.Write("Введите корректное число N!");
+}
+else
+{
+    Console.Write("Числа от 1 до " + N + ": ");
+    for (int i = 1; i <= N; i++)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write(i + " ");
+        }
+        else Console.Write("Чётных чисел нет"); // В случае, если ввести N = 1.
+    }
+}
