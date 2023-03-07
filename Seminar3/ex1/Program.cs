13522b40c9124446ea2b280431770a684029d987
Viktoriya <kuzmina.vika2012@yandex.ru>
2023-03-07 15:46:44 +0300

    Палиндром

---
+++ Seminar3/ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите 5ти значное число:\t");
+int number = Convert.ToInt32(Console.ReadLine());
+
+var first = number / 10000;
+var second = number / 1000 % 10;
+var first_end = number % 10;
+var second_end = number % 100 / 10;
+
+if (first == first_end && second == second_end)
+{
+    Console.WriteLine("Число является палиндромом");
+} 
+else
+    Console.WriteLine("Число НЕ является палиндромом");
