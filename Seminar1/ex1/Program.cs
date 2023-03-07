6d4523438c5b9e738aaa4a83bb4bc15b0335dd83
Viktoriya <kuzmina.vika2012@yandex.ru>
2023-03-07 15:15:02 +0300

    Поиск максимального и минимального среди двух чисел

---
+++ Seminar1/ex1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число: ");
+double first = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите второе число: ");
+double second = Convert.ToDouble(Console.ReadLine());
+
+if (first > second) {
+Console.WriteLine("Максимальное число = " + first + ", минимальное число = " + second);
+}
+else if (first < second){
+Console.WriteLine("Максимальное число = " + second + ", минимальное число = " + first);
+}
+else {
+    Console.WriteLine("Числа равны между собой");
+}
