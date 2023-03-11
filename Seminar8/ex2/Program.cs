60814a4269bea4a0ccc0cf8323f8450f94966607
Viktoriya <kuzmina.vika2012@yandex.ru>
2023-03-11 15:36:47 +0300

    Найти номер строки с минимальной суммой элементов в этой строке

--- Seminar8/ex2/Program.cs
+++ Seminar8/ex2/Program.cs
@@ -57,7 +57,6 @@ int[,] InputMatrix(int rows, int column)
 /// <param name="matrix">Заполненный массив</param>
 void OutputMatrix(int[,] matrix)
 {
-    Console.WriteLine("Массив ДО перестановки: ");
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
