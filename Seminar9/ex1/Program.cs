7874f9073a2ae08563c8f1c99c623340a3f335dd
Viktoriya <kuzmina.vika2012@yandex.ru>
2023-03-13 03:56:21 +0300

    Вывод чисел от N до 1

---
+++ Seminar9/ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumber(int start){
+    if (start == 1) return start.ToString();
+    return (start + "," + PrintNumber(start - 1));
+}
+
+Console.WriteLine(PrintNumber(N));
